b527653dd25cd564c9e65b9f113ecf0dcf2ae30f
王家俊 <mrblack8922@outlook.com>
2017-01-19 18:18:23 +0800

    Initial Commit

---
+++ JJDetailServiceProtocol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JJDetailServiceProtocol
+//
+//  Created by 王家俊 on 2017/1/19.
+//  Copyright © 2017年 KEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
